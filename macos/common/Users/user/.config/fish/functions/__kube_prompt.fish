75f87a32e369eabdddc2df42168844028008767d
Gintaras <91011496+ganiulis@users.noreply.github.com>
2024-02-23 09:26:59 +0200

    Fish shell for macOS (#4)
    * Fish shell for macOS
    
    * Add README.md for macOS
    
    * Add shell script for Fish
    
    * Add local alias directory for Fish shell
    
    * Update Fish prompt

---
+++ macos/common/Users/user/.config/fish/functions/__kube_prompt.fish
@@ -0,0 +1,57 @@
+function __kube_ps_update_cache
+  function __kube_ps_cache_context
+    set -l ctx (kubectl config current-context 2>/dev/null)
+    if test $status -eq 0
+      set -g __kube_ps_context "$ctx"
+    else
+      set -g __kube_ps_context "n/a"
+    end
+  end
+
+  function __kube_ps_cache_namespace
+    set -l ns (kubectl config view --minify -o 'jsonpath={..namespace}' 2>/dev/null)
+    if test -n "$ns"
+      set -g __kube_ps_namespace "$ns"
+    else
+      set -g __kube_ps_namespace "default"
+    end
+  end
+
+  function __stat_mtime
+    python3 -c "print(__import__('os').stat(__import__('sys').argv[1]).st_mtime)" $argv
+  end
+
+  set -l kubeconfig "$KUBECONFIG"
+  if test -z "$kubeconfig"
+    set kubeconfig "$HOME/.kube/config"
+  end
+
+  if test "$kubeconfig" != "$__kube_ps_kubeconfig"
+    __kube_ps_cache_context
+    __kube_ps_cache_namespace
+    set -g __kube_ps_kubeconfig "$kubeconfig"
+    set -g __kube_ps_timestamp (date +%s)
+    return
+  end
+
+  for conf in (string split ':' "$kubeconfig")
+    if test -r "$conf"
+      if test -z "$__kube_ps_timestamp"; or test (__stat_mtime "$conf") -gt "$__kube_ps_timestamp"
+        __kube_ps_cache_context
+        __kube_ps_cache_namespace
+        set -g __kube_ps_kubeconfig "$kubeconfig"
+        set -g __kube_ps_timestamp (date +%s)
+        return
+      end
+    end
+  end
+end
+
+function __kube_prompt
+  if test -z "$__kube_ps_enabled"; or test $__kube_ps_enabled -ne 1
+    return
+  end
+
+  __kube_ps_update_cache
+  echo -n -s "$__kube_ps_context ($__kube_ps_namespace) "
+end
---
+++ macos/common/Users/user/.config/fish/functions/ciao.fish
@@ -0,0 +1,4 @@
+function ciao --wraps=exit --description 'alias ciao=exit'
+  exit $argv
+        
+end
