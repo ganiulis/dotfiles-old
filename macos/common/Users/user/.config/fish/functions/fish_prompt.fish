4397186a6ed9bab3abaecd260d38e9fe1d1f16db
Gintaras Aniulis <gintarasa95@gmail.com>
2024-02-23 09:38:22 +0200

    Update Fish prompt

--- macos/common/Users/user/.config/fish/functions/fish_prompt.fish
+++ macos/common/Users/user/.config/fish/functions/fish_prompt.fish
@@ -1,3 +1,7 @@
 function fish_prompt
-  echo -s (set_color bryellow) (whoami) " " (set_color brgreen) (prompt_pwd) (set_color brred) (fish_git_prompt) " " (set_color brblue) (__kube_prompt)  (set_color $fish_color_normal)"> "
+    echo -s (set_color yellow) (whoami) " " (set_color green) (prompt_pwd) (set_color red) (fish_git_prompt) " " (set_color $fish_color_normal)"> "
+end
+
+function fish_right_prompt
+    echo -s (set_color blue) (__kube_prompt) (set_color normal) " " (date +'%m-%d %T')
 end
