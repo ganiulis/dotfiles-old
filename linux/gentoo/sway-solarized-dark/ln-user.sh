786988745eb7e7b54a2927062ec2602ca0e5f85e
Gintaras Aniulis <gintarasa95@gmail.com>
2024-02-23 14:36:19 +0200

    Add script to symlink regular user configs

--- linux/gentoo/sway-solarized-dark/ln-user.sh
+++ linux/gentoo/sway-solarized-dark/ln-user.sh
@@ -2,15 +2,15 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-for APP in foot sway waybar wofi; do
-  CONFIG_DIR=$SCRIPT_DIR/home/user/.config/$APP
-  TARGET_DIR=~/.config/$APP
+for APP in .config/foot .config/sway .config/waybar .config/wofi .local/share/applications .local/share/fonts .local/share/icons; do
+  CONFIG_DIR=$SCRIPT_DIR/home/user/$APP
+  TARGET_DIR=~/$APP
 
   if [ -e $TARGET_DIR ]; then
     if [ -L $TARGET_DIR ]; then
 	    rm $TARGET_DIR
 
-	    echo 'Removed current symlink for '$APP
+	    echo 'Removed current symlink for '$TARGET_DIR
     else
   	  if [ -e $TARGET_DIR-old ]; then
 	  	  rm -rf $TARGET_DIR-old
@@ -18,13 +18,13 @@ for APP in foot sway waybar wofi; do
 
       mv $TARGET_DIR $TARGET_DIR-old
 
-	    echo 'Moved existing '$APP' config to '$TARGET_DIR'-old'
+	    echo 'Moved existing '$TARGET_DIR' config to '$TARGET_DIR'-old'
     fi
   fi
 
   if [ ! -e $TARGET_DIR ]; then
 	  ln -s $CONFIG_DIR $TARGET_DIR
 
-  	echo 'Symlinked configs for '$APP
+  	echo 'Symlinked configs for '$TARGET_DIR
   fi
 done
